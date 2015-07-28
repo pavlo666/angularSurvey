6e6c90944fe41ac11ff9cc98e80e2e91018dc63b
Pavlo <galanpol@ukr.net>
2015-07-28 16:59:11 +0300

    surveyController with surveyPage bindings.

--- client/js/app.js
+++ client/js/app.js
@@ -8,10 +8,10 @@
     angularModule.config(['$routeProvider', function($routeProvider) {
         $routeProvider.
             when("/surveys", {templateUrl: "partials/surveyList.html"}).
-            when("/surveys:id", {templateUrl: "partials/surveyPage.html"}).
-            when("/question:id", {templateUrl: "partials/questionPage.html"}).
-            when("/results:id", {templateUrl: "partials/results.html"}).
-            when("/edit:id", {templateUrl: "partials/addSurvey.html"}).
+            when("/surveys/:id", {templateUrl: "partials/surveyPage.html"}).
+            when("/question/:id", {templateUrl: "partials/questionPage.html"}).
+            when("/results/:id", {templateUrl: "partials/results.html"}).
+            when("/edit/:id", {templateUrl: "partials/addSurvey.html"}).
             when("/create", {templateUrl: "partials/addSurvey.html"}).
             when("/used", {templateUrl: "partials/used.html"}).
             otherwise({redirectTo: '/surveys'});
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -7,24 +7,32 @@
 
     var angularModule = angular.module("SRV");
 
+    var surveyMock = [{
+        id : "1",
+        name: "Office facilities",
+        description : "Provide feedback on office facilities such as place, furniture, cleaning, etc."
+    },{
+        id : "2",
+        name: "Finance services",
+        description : "Provides feedback on finance department work - compensation and other stuff"
+    },{
+        id : "3",
+        name: "IT service",
+        description : "Provide feedback on IT department work"
+    },{
+        id : "4",
+        name: "HR services",
+        description : "Provides feedback on HR department work - people hiring and people partners work."
+    }];
+
     angularModule.controller("surveysListController", function ($scope) {
-        $scope.surveys = [{
-                id : "1",
-                name: "Office facilities",
-                description : "Provide feedback on office facilities such as place, furniture, cleaning, etc."
-            },{
-                id : "2",
-                name: "Finance services",
-                description : "Provides feedback on finance department work - compensation and other stuff"
-            },{
-                id : "3",
-                name: "IT service",
-                description : "Provide feedback on IT department work"
-            },{
-                id : "4",
-                name: "HR services",
-                description : "Provides feedback on HR department work - people hiring and people partners work."
-        }];
+        $scope.surveys = surveyMock;
+    });
+
+    angularModule.controller("surveyController", function($scope, $routeParams){
+        $scope.survey = _.find(surveyMock, function(item){
+            return item.id === $routeParams.id;
+        });
     });
 
 })();
