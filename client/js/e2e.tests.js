a924a9a2ec55cabfe45bfa0e70a8a52f87f73272
Pavlo <galanpol@ukr.net>
2015-08-05 14:12:32 +0300

    one more simple test for protractor

--- client/js/e2e.tests.js
+++ client/js/e2e.tests.js
@@ -1,10 +1,11 @@
 
-/* global describe: true, beforeEach: true, inject: true, it: true, module: true, expect: true*/
+/* global describe: true, beforeEach: true, inject: true, it: true, module: true, expect: true, browser: true, by: true, element: true*/
 
 
 (function () {
     "use strict";
     describe('survey homepage', function() {
+
         it('should redirect index.html to index.html#/surveys', function () {
             browser.get('http://localhost:8123/index.html');
             browser.getLocationAbsUrl().then(function (url) {
@@ -13,6 +14,12 @@
         });
 
         it('used menu item should redirect to index.html#/used', function(){
+
+            element(by.id('used')).click();
+
+            browser.getLocationAbsUrl().then(function (url) {
+                expect(url).toEqual('/used');
+            });
 
         });
     });
