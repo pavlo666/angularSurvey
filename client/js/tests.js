6f4e40e7056b2500d87cfa0094a1c983de26efe7
Pavlo <galanpol@ukr.net>
2015-08-05 12:39:20 +0300

    Karma tests combing

--- client/js/tests.js
+++ client/js/tests.js
@@ -1,4 +1,4 @@
-
+/* global describe: true, beforeEach: true, inject: true, it: true, module: true, expect: true*/
 
 (function () {
     "use strict";
@@ -8,7 +8,6 @@
             var $controller;
 
             beforeEach(inject(function(_$controller_){
-                // The injector unwraps the underscores (_) from around the parameter names when matching
                 $controller = _$controller_;
             }));
 
@@ -16,7 +15,7 @@
                 it('returns 0 for the first question of survey', function() {
                     var $scope = {};
                     var $routeParams = {sid : 0, qid: 0};
-                    var controller = $controller('questionController', { $scope: $scope, $routeParams: $routeParams });
+                    $controller('questionController', { $scope: $scope, $routeParams: $routeParams });
                     expect($scope.getProgress()).toEqual(0);
                 });
             });
